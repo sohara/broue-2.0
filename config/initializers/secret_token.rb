2b577759b8866d679fb03ef84363baa7467c9643
Sean O'Hara <sohara@sohara.com>
2014-02-22 14:51:27 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Broue::Application.config.secret_key_base = 'b9295cfaa6ff2dd5cdcaf50261098596dae7bca3e07273a17c7ed053e46c01d0e489cf08fd9a5cfdc8d6201b21a1104340b83543d916805f160a4ab349b0e2b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Broue::Application.config.session_store :cookie_store, key: '_broue_session'
